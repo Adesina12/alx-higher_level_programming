52fb9d2c143728aaad9536d64861874b8e84754e
daadesina <daadesina1@gmail.com>
2024-01-04 17:33:22 +0100

    adesina

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,5 +4,12 @@ if __name__ == "__main__":
     out = sys.argv
     outLen = range(1, len(sys.argv))
 
+    if (len(sys.argv) - 1) == 1:
+        print("{} argument:".format(len(sys.argv) - 1))
+    elif (len(sys.argv) - 1) > 1:
+        print("{} arguments:".format(len(sys.argv) - 1))
+    else:
+        print("{} argument.".format(len(sys.argv) - 1))
+
     for i in outLen:
         print("{}: {}".format(i, out[i]))
