9059835361226ab904cb892ad83d77953427284f
daadesina <daadesina1@gmail.com>
2024-02-05 20:49:24 +0100

    adesina

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -40,3 +40,6 @@ class Square(Rectangle):
         self.__size = size
 
         super().__init__(self.__size, self.__size)
+
+    def __str__(self):
+        return (f"[{type(self).__name__}] {self.__size}/{self.__size}")
