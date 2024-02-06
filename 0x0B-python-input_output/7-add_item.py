263c8868bc7b34b4a3b862234b4cf4c8bd7974b9
daadesina <daadesina1@gmail.com>
2024-02-06 23:28:03 +0100

    adesina

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,14 +1,11 @@
 #!/usr/bin/python3
-'''A module'''
-
-
+"""Load, add, save"""
 from sys import argv
 from os.path import exists
-import json
 
 
-save_to_json_file = __import__('save_to_json_file').save_to_json_file
-load_from_json_file = __import__('load_from_json_file').load_from_json_file
+save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
+load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
 
 namefile = "add_item.json"
 argc = len(argv)
