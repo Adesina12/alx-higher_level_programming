41b634f4e3cc12d36981bed5b0e4665371c77061
daadesina <daadesina1@gmail.com>
2024-04-20 23:15:32 +0100

    adesina

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+'''Select States'''
+# 1. import
+# 2. connect
+# 3. cursor
+# 4. execute
+# 5. fectchall
+# 6. close
+
+import mysql.connector
+from mysql.connector import Error
+import sys
+import MySQLdb
+
+try:
+    connection = mysql.connector.connect(
+            user=sys.argv[1],
+            host='localhost',
+            password=sys.argv[2],
+            database=sys.argv[3],
+            port=3306,
+            )
+    if connection.is_connected():
+        cursor = connection.cursor()
+        cursor.execute('SELECT * FROM states;')
+        record = (cursor.fetchall())
+        for num in record:
+            print(num)
+
+except Error as e:
+    print("The Error is ", e)
+finally:
+    if connection.is_connected():
+        cursor.close()
+        connection.close()
