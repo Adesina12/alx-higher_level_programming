71a30611561d2cf88186e886a7dd119e069df4e2
daadesina <daadesina1@gmail.com>
2024-04-21 07:08:05 +0100

    adesina

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -7,20 +7,25 @@ from mysql.connector import Error
 import sys
 """python3 -c 'print(__import__("my_module").__doc__)'"""
 
-connection = mysql.connector.connect(
-        user=sys.argv[1],
-        host='localhost',
-        password=sys.argv[2],
-        database=sys.argv[3],
-        port=3306,
-        )
-if connection.is_connected():
-    cursor = connection.cursor()
-    cursor.execute('SELECT * FROM states;')
-    record = (cursor.fetchall())
-    for num in record:
-        print(num)
+def my_func()
+    """python3 -c 'print(__import__("my_module").__doc__)'"""
+    connection = mysql.connector.connect(
+            user=sys.argv[1],
+            host='localhost',
+            password=sys.argv[2],
+            database=sys.argv[3],
+            port=3306,
+            )
+    if connection.is_connected():
+        cursor = connection.cursor()
+        cursor.execute('SELECT * FROM states;')
+        record = (cursor.fetchall())
+        for num in record:
+            print(num)
 
-if connection.is_connected():
-    cursor.close()
-    connection.close()
+    if connection.is_connected():
+        cursor.close()
+        connection.close()
+
+if __name__=="__main__":
+    my_func()
